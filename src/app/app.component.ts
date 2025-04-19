f0016271860cf95c7b9e02ee6cff118041ab4c90
ayman-mostafa-othman <ayman.eldelil@gmail.com>
2025-04-20 00:49:51 +0200

    VERTICAL_TIMELINE_MOCKS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { VerticalTimelineComponent } from './vertical-timeline/vertical-timeline.component';
+import { IVerticalTimelineItem } from './vertical-timeline/models/interfaces/vertical-timeline.interface';
+import { ICONS } from './icon/models/icon.const';
+import { LogoStripComponent } from './logo-strip/logo-strip.component';
+import { LOGO_STRIP_MOCK } from './logo-strip/models/const/logo-strip.const';
+import { ILogoStrip } from './logo-strip/models/interfaces/logo-strip.interface';
+import { VERTICAL_TIMELINE_MOCKS } from './vertical-timeline/models/const/vertical-timeline.const';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [VerticalTimelineComponent, LogoStripComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'vertical-timeline';
+  verticalTimelineList: Array<IVerticalTimelineItem> = VERTICAL_TIMELINE_MOCKS;
+
+  logoStripList: ILogoStrip[] = LOGO_STRIP_MOCK;
 }
